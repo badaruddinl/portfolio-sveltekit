4f2f98b3c51fafae15336f46dee5987fa039ece0
Badaruddin Luthfi <90174228@bri.co.id>
2025-07-02 12:46:55 +0700

    change state

--- src/lib/components/NavItems.svelte
+++ src/lib/components/NavItems.svelte
@@ -4,16 +4,9 @@
 
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 
 	export let items: { label: string; href: string }[] = [];
-
-	// Reactive path tracking
-	$: currentPath = $page.url.pathname;
-
-	function navigate(href: string) {
-		goto(href);
-	}
 </script>
 
 <nav class="flex items-center justify-between rounded-b-md bg-amber-300 px-4">
@@ -24,10 +17,10 @@
 				<button
 					on:click={() => goto(item.href)}
 					class="w-full cursor-pointer px-3 py-4 text-left text-sm transition-all delay-100 duration-300 hover:bg-yellow-500"
-					class:font-normal={currentPath !== item.href}
-					class:font-extrabold={currentPath === item.href}
-					class:bg-yellow-500={currentPath === item.href}
-					class:text-white={currentPath === item.href}
+					class:font-normal={page.url.pathname !== item.href}
+					class:font-extrabold={page.url.pathname === item.href}
+					class:bg-yellow-500={page.url.pathname === item.href}
+					class:text-white={page.url.pathname === item.href}
 				>
 					<span class="font-semibold">
 						{item.label}
