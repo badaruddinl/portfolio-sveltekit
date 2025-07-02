eab478329a0d788cabdeca8d6a7974df6b3f9a16
Badaruddin Luthfi <90174228@bri.co.id>
2025-07-02 12:48:51 +0700

    fix: remove unused declared

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, integer, text, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, integer, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
 	id: text('id').primaryKey(),
