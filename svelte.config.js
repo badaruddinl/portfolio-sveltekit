8d12312ffc61b298abf3dfa8b2ca0c551d90d4c4
Badaruddin Luthfi <badarlut@gmail.com>
2025-07-27 14:58:57 +0700

    feat: add adaptor node

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess({
 		postcss: true
 	}),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			out: 'build'
+		})
+	}
 };
-
 export default config;
